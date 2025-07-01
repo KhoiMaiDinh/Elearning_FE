a624652730a3393783e5fbfde3d5fea521c50ab9
2122059 <21522059@gm.uit.edu.vn>
2025-07-02 00:41:42 +0700

    add notificationr

--- fe_elearning/src/components/notifications/notificationComponent.tsx
+++ fe_elearning/src/components/notifications/notificationComponent.tsx
@@ -224,7 +224,9 @@ function NotificationItem({
                     ? `/profile/lecture?tab=ho-so&rejected=true&reason=${notification?.body}`
                     : type === 'NEW_THREAD'
                       ? `/course-details/${notification?.metadata?.course_id}?lecture=${notification?.metadata?.lecture_id}&thread=${notification?.metadata?.thread_id}&tab=community`
-                      : '',
+                      : type === 'COURSE_UPDATED'
+                        ? `/course-details/${notification?.metadata?.course_id}`
+                        : '',
             '_blank'
           );
           handleReadNotification(notification?.id);
